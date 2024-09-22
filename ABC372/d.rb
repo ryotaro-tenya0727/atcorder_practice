7d19afe18b1ccd9419a7ecdfbe3af9765840a3b5
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2024-09-23 00:31:39 +0900

    ADD: ABC372のDの解答追加

--- ABC372/d.rb
+++ ABC372/d.rb
@@ -1,19 +1,12 @@
-N=gets.to_i
-H=gets.split.map(&:to_i)
-sums=[0, 0]
-for i in 2..N-1 do
-  count=0
-  for k in 1..i-1 do
-    if H[k] < H[i]
-      count+=1
-    end
-  end
-  sums << count
+N = gets.to_i
+H = gets.split.map(&:to_i)
+s = []
+ans = []
+
+H.reverse_each do |h|
+  ans << s.size
+  s.pop while s.any? && s.last < h
+  s << h
 end
-ans << sums[N-1]
-for i in (1..N-1) do
-  if H[i] < H[i-1]
-    ans << sums[i]
-  end
-end
-# puts ans.join(" ")
+
+puts ans.reverse.join(" ")
