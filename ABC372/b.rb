56c5b214672030db7a65152382fcd38164132f4c
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2024-09-22 23:00:24 +0900

    ADD: ABC372のBの解答追加

--- ABC372/b.rb
+++ ABC372/b.rb
@@ -1,1 +1,11 @@
-N=gets.to_i
+M=gets.to_i.to_s(3).split("").map(&:to_i).reverse
+ans = []
+for i in (0..10) do
+  next if M[i].nil?
+  M[i].times do
+    ans << i
+  end
+end
+
+puts ans.size
+puts ans.join(" ")
